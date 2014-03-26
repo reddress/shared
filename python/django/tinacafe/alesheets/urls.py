5b5474e5fd81b3476a06815a138776eaa51e44b7
Heitor Chang <hcproradio@gmail.com>
2014-03-26 17:01:18 -0300

    fix links

--- python/django/tinacafe/alesheets/urls.py
+++ python/django/tinacafe/alesheets/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = patterns('',
                        url(r'^$', views.index),
                        url(r'^account/(?P<short_name>\S+)/$', views.show_account),
+                       url(r'^balances/$', views.show_balances),
                        url(r'^login/$', login),
                        url(r'^dologin/$', views.user_login),
                        url(r'^logout/$', views.user_logout),
--- python/django/tinacafe/alesheets/views.py
+++ python/django/tinacafe/alesheets/views.py
@@ -10,18 +10,18 @@ from .models import Owner, AccountType, Account, Transaction
 
 @login_required
 def index(request):
-    asset_accounts = Account.objects.filter(type__name="Asset")
-    expense_accounts = Account.objects.filter(type__name="Expense")
-    liability_accounts = Account.objects.filter(type__name="Liability")
-    equity_accounts = Account.objects.filter(type__name="Equity")
-    income_accounts = Account.objects.filter(type__name="Income")
+    asset_accounts = Account.objects.filter(type__name="Asset").order_by('short_name')
+    expense_accounts = Account.objects.filter(type__name="Expense").order_by('short_name')
+    liability_accounts = Account.objects.filter(type__name="Liability").order_by('short_name')
+    equity_accounts = Account.objects.filter(type__name="Equity").order_by('short_name')
+    income_accounts = Account.objects.filter(type__name="Income").order_by('short_name')
 
     return render(request, 'alesheets/index.html',
-                  { 'asset_accounts': asset_accounts,
-                    'expense_accounts': expense_accounts,
-                    'liability_accounts': liability_accounts,
-                    'equity_accounts': equity_accounts,
-                    'income_accounts': income_accounts })
+                  {'asset_accounts': asset_accounts,
+                   'expense_accounts': expense_accounts,
+                   'liability_accounts': liability_accounts,
+                   'equity_accounts': equity_accounts,
+                   'income_accounts': income_accounts})
 
 def get_transaction_html(transaction, account, balance):
     sign = account.type.sign_modifier
@@ -60,11 +60,11 @@ def get_transaction_html(transaction, account, balance):
         
 @login_required
 def show_account(request, short_name):
-    asset_accounts = Account.objects.filter(type__name="Asset")
-    expense_accounts = Account.objects.filter(type__name="Expense")
-    liability_accounts = Account.objects.filter(type__name="Liability")
-    equity_accounts = Account.objects.filter(type__name="Equity")
-    income_accounts = Account.objects.filter(type__name="Income")
+    asset_accounts = Account.objects.filter(type__name="Asset").order_by('short_name')
+    expense_accounts = Account.objects.filter(type__name="Expense").order_by('short_name')
+    liability_accounts = Account.objects.filter(type__name="Liability").order_by('short_name')
+    equity_accounts = Account.objects.filter(type__name="Equity").order_by('short_name')
+    income_accounts = Account.objects.filter(type__name="Income").order_by('short_name')
     
     account = get_object_or_404(Account, short_name=short_name)
     debit_transactions = Transaction.objects.filter(debit=account,
@@ -82,16 +82,54 @@ def show_account(request, short_name):
         balance = row_data[1]
         
     return render(request, 'alesheets/showaccount.html',
-                  { 'account_name': account.name,
-                    'account_short_name': account.short_name,
-                    'transactions': all_transactions,
-                    'asset_accounts': asset_accounts,
-                    'expense_accounts': expense_accounts,
-                    'liability_accounts': liability_accounts,
-                    'equity_accounts': equity_accounts,
-                    'income_accounts': income_accounts })
-                    
+                  {'account_name': account.name,
+                   'account_short_name': account.short_name,
+                   'transactions': all_transactions,
+                   'asset_accounts': asset_accounts,
+                   'expense_accounts': expense_accounts,
+                   'liability_accounts': liability_accounts,
+                   'equity_accounts': equity_accounts,
+                   'income_accounts': income_accounts})
 
+def get_balance(account):
+    balance = 0
+    debit_transactions = Transaction.objects.filter(debit=account,
+                                date__gt=(datetime.today()-timedelta(days=31)))
+    credit_transactions = Transaction.objects.filter(credit=account,
+                                date__gt=(datetime.today()-timedelta(days=31)))
+    raw_transactions = sorted(chain(debit_transactions,
+                                    credit_transactions),
+                              key=lambda tr: tr.date)
+    for transaction in raw_transactions:
+        sign = account.type.sign_modifier
+        if str(transaction.credit) == str(account.short_name):
+            sign *= -1
+        balance += sign * transaction.value
+    return '<tr><td><a href="/alesheets/account/%s/">%s</a></td><td>%.2f</td></tr>' % (account.short_name, account.short_name, balance)
+
+@login_required
+def show_balances(request):
+    asset_accounts = Account.objects.filter(type__name="Asset").order_by('short_name')
+    expense_accounts = Account.objects.filter(type__name="Expense").order_by('short_name')
+    liability_accounts = Account.objects.filter(type__name="Liability").order_by('short_name')
+    equity_accounts = Account.objects.filter(type__name="Equity").order_by('short_name')
+    income_accounts = Account.objects.filter(type__name="Income").order_by('short_name')
+
+    account_types = [asset_accounts, expense_accounts, liability_accounts,
+                     equity_accounts, income_accounts]
+    type_balances = []
+    for type in account_types:
+        type_balances.append(map(get_balance, type))
+        
+    #accounts = Account.objects.all()
+    #balances = map(get_balance, accounts)
+    return render(request, 'alesheets/showbalances.html',
+                  {'type_balances': type_balances,
+                   'asset_accounts': asset_accounts,
+                   'expense_accounts': expense_accounts,
+                   'liability_accounts': liability_accounts,
+                   'equity_accounts': equity_accounts,
+                   'income_accounts': income_accounts})
 
 def user_login(request):
     username = request.POST['username']
