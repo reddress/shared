891732d49e3a68234ae56ea36f441261f85be88a
Heitor Chang <hcproradio@gmail.com>
2014-04-28 17:25:14 -0300

    learn sql the hard way

---
+++ sql/ex3.sql
@@ -0,0 +1,10 @@
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (0, "Zed", "Shaw", 37);
+
+INSERT INTO pet (id, name, breed, age, dead)
+  VALUES (0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
+
+INSERT INTO pet (id, age, name, breed, dead)
+  VALUES (2, 3, "Ziggy", "Snorfblat", 0);
---
+++ sql/ex9.sql
@@ -0,0 +1,9 @@
+UPDATE person SET first_name ='Hilarious Guy', last_name = 'Esq.'
+  WHERE first_name = 'Zed';
+
+UPDATE pet SET name = 'Fancy Pants'
+  WHERE id = 0;
+
+SELECT * FROM person;
+SELECT * FROM pet;
+
