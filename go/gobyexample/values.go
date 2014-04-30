5a7b97f92ff7843e23831df8b592dcb6b146bb9b
Heitor Chang <hcproradio@gmail.com>
2014-04-30 17:46:39 -0300

    add go

---
+++ go/gobyexample/values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Go" + "lang")
+  fmt.Println("1+1 = ", 1+1)
+  fmt.Println("7/3 = ", 7/3)
+  fmt.Println("7.0/3.0=", 7.0/3.0)
+  fmt.Println("0.1 x 3=", 0.1 + 0.1 + 0.1)
+  fmt.Println(true || false)
+}
---
+++ python/lantana/timeconv.py
@@ -0,0 +1,6 @@
+# get an unit of time as integer
+
+from datetime import datetime
+import time
+t = int(time.time())
+print(t)
