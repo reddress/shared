18b1aae6f0d7a80ec7ab2f74ec3c9270e04d18fa
Heitor Chang <hcproradio@gmail.com>
2014-06-02 15:16:44 -0300

    begin kivy book

---
+++ python/mykivy/events/main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class AddLocationForm(BoxLayout):
+    pass
+
+class WeatherApp(App):
+    pass
+
+if __name__ == '__main__':
+    WeatherApp().run()
---
+++ python/mykivy/helloworld/main.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+
+# App().run()
+
+class WeatherApp(App):
+    pass
+
+if __name__ == '__main__':
+    WeatherApp().run()
+
+    
+
