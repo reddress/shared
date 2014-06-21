ed36e23519820d571fd8d217bd81c6f631818fbf
Heitor C <hcproradio@gmail.com>
2014-06-20 22:52:21 -0300

    begin learning sql

---
+++ python/sql/gettingstarted.py
@@ -0,0 +1,12 @@
+# open cmd
+# cd \apache24
+# httpd.exe
+
+import mysql.connector
+cnx = mysql.connector.connect(user='tina', password='tina3', host='127.0.0.1', database='mydb')
+
+cursor.execute("SELECT * FROM corp;")  # result is an iterable
+list(cursor)
+
+cursor.execute("INSERT INTO corp (corp_id, name) VALUES (12, 'Incorp');")
+cnx.commit()
