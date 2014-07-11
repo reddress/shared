0b4f428083a8efffe5b8530b7a3863b8b2525bf0
Heitor Chang <hcproradio@gmail.com>
2014-07-11 17:15:38 -0300

    minor edit

--- java/learning/Scratch.java
+++ java/learning/Scratch.java
@@ -1,30 +1,23 @@
 import java.util.*;
 
+class Cat {
+    String name;
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public static void meow() {
+        System.out.println("Miau!");
+    }
+}
+
 public class Scratch {
     public static <T> void print(T val) {
         System.out.println(val);
     }
 
     public static void main(String[] args) {
-        print("Hello");
-        int foo;
-        int bar = 3;
-        boolean baz = false;
-        if(baz = true) {  // compiler does not flag unintentional assignment
-            foo = 42;
-            print(foo);
-        }
-        Float f = new Float("3.0");
-        print(f.longValue());
-
-        Integer i = 1234567890;
-        Integer j = 1234567890;
-        print(i == j);  // false
-
-        print(i.equals(j));  // true
-        
-        Integer n = 2;
-        Integer m = 2;
-        print(n == m);  // true because value is small (< 127)
+        Cat kat = new Cat("Kat");
+        kat.meow();
     }
 }
