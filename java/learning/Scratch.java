513c021e8e329d397d9578f55d4070592916f3e7
Heitor C <hcproradio@gmail.com>
2014-07-13 21:09:30 -0300

    overloading

--- java/learning/Scratch.java
+++ java/learning/Scratch.java
@@ -4,10 +4,6 @@ import java.text.*;
 
 public class Scratch {
     public static void main(String[] args) {
-        Cat kat = new Cat("Kat");
-        kat.meow();
-        print(55);
-        System.out.printf("pos num +%d %n", 1435029199);
-        print(MessageFormat.format("{0} + {0} = {1}", 3, 4, 7));
+
     }
 }
