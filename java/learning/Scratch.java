127a0e09e3dbfc16d4c83f506bb60c7f38771f08
Heitor Chang <hcproradio@gmail.com>
2014-07-14 17:40:53 -0300

    inheritance

--- java/learning/Scratch.java
+++ java/learning/Scratch.java
@@ -1,9 +1,50 @@
-import java.util.*;
 import static myutil.Convenience.print;
-import java.text.*;
+
+class Animal {
+    float weight;
+    Animal(float weight) {
+        this.weight = weight;
+    }
+
+    float getWeight() {
+        return this.weight;
+    }
+    
+    void eat() {
+        print("OM NOM NOM");
+    }
+}
+
+class Mammal extends Animal {
+    int heartRate;
+
+    Mammal(float weight) {
+        super(weight);
+    }
+
+    void breathe() {
+        print("breathing...");
+    }
+}
+
+class Cat extends Mammal {
+    Cat(float weight) {
+        super(weight);
+    }
+    
+    boolean longHair;
+    void purr() {
+        print("Purrrrrrrrrr");
+    }
+}
 
 public class Scratch {
     public static void main(String[] args) {
+        Cat simon = new Cat(92.1f);
+        // Animal creature = new Animal(9.2);
+        Animal creature = simon;
 
+        simon.purr();
+        print(creature.getWeight());
     }
 }
