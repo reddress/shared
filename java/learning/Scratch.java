9add97b1667ce5ed37f5a572f8be9f7ab21249b2
Heitor C <hcproradio@gmail.com>
2014-08-12 23:01:04 -0300

    zero division

--- java/learning/Scratch.java
+++ java/learning/Scratch.java
@@ -4,6 +4,16 @@ import java.util.*;
 
 public class Scratch {
     public static void main(String[] args) {
-        print(args[0]);
+        print(inverse(Double.parseDouble(args[0])));
+    }
+
+    public static double inverse(double x) {
+        try {
+            return 1.0 / x;
+        }
+        catch (Exception e) {
+            print("caught object");
+            return 0;
+        }
     }
 }
