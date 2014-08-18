00b2cc627a3b626f40586bc62a6addd28a6c6a62
Heitor Chang <hcproradio@gmail.com>
2014-08-18 17:42:16 -0300

    cookbook strings

--- java/cookbook/src/main/java/com/tinacg/MyMainMan.java
+++ java/cookbook/src/main/java/com/tinacg/MyMainMan.java
@@ -2,18 +2,18 @@ package com.tinacg;
 
 import static com.tinacg.util.Shortcuts.print;
 
-import java.io.File;
+import java.io.*;
 
 public class MyMainMan {
     // in directory c:\Users\Heitor\Desktop\emacs-24.3\bin\shared\java\cookbook
     // > gradle build
-
+    
     // If jar is activated in build.gradle
     // > java -ea -jar build/libs/cookbook-0.1.jar
 
     // otherwise
     // run.bat
-
+    
     // shorter:
     // compile.bat then run.bat
     
@@ -23,9 +23,25 @@ public class MyMainMan {
     */
     
     public static void main(String[] args) {
+        // PROGRESS: p. 83
+
+        /* p. 82
+        try {
+            print(System.getProperty("user.dir"));
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream("cookbookunicode.html"), "UTF-8"));
+            pw.write("<html><head><meta charset=\"utf-8\"></head><body>");
+            pw.write("\u00a5\u01fc\u0391\u03a9");
+            pw.close();
+        }
+        catch (Exception e) { }
+        */
+        
+        /* p. 56
         System.out.println(System.getProperty("java.specification.version"));
         print(File.separator);
         print(File.pathSeparatorChar);
+        */
+        
         // p. 35 1.11 AssertDemo
         /*
         int i = 4;
