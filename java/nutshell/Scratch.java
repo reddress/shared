7b23c6168af4a6f2fe4b9764fd0bb6b6cbf92abb
Heitor Chang <hcproradio@gmail.com>
2014-08-22 17:44:25 -0300

    references

--- java/nutshell/Scratch.java
+++ java/nutshell/Scratch.java
@@ -1,11 +1,55 @@
 import java.util.*;
 import java.io.*;
 
+class Dog {
+    public void answer() {
+        System.out.println("Yes this is dog");
+    }
+}
+
+class Point {
+    public double x, y;
+    public Point(double x, double y) {
+        this.x = x; this.y = y;
+    }
+    public double sum() {
+        return this.x + this.y;
+    }
+    public void setX(double newx) {
+        this.x = newx;
+    }
+}
+    
 class Scratch {
-    public static void main(String[] args) throws InterruptedException {
-        countdown(9);
+    public static void main(String[] args) {
+        Point origin = new Point(0, 0);
+        Point[] triangle = new Point[3];
+        triangle[0] = origin;
+        triangle[1] = new Point(3, 10);
+        triangle[2] = new Point(0, 3);
+
+        Point[] triclone = triangle.clone();
+
+        triclone[1].setX(2.0);
+        print(triangle[1].sum());
+
+        byte[] a = { 1, 2, 3 };
+        byte[] b = (byte[]) a.clone();
+
+        int aa = 290138814;
+        int bb = 290138814;
+        if ("abc".equals("abc")) {
+            print("equal");
+        }
+
+        Dog d = new Dog();
+        d.answer();
     }
 
+    public static void setHeadToZero(int[] arr) {
+        arr[0] = 0;
+    }
+        
     public static void countdown(int from) throws InterruptedException {
         for (; from > 0; from--) {
             Thread.sleep(200);
