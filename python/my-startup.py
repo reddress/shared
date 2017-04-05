c23cf95f6316937d0c1b8ed01f009d8579954080
Heitor Chang <heitorchang@gmail.com>
2017-04-05 14:57:26 -0300

    testeql

--- python/my-startup.py
+++ python/my-startup.py
@@ -1,7 +1,7 @@
 from math import exp, log, sin, cos, tan, asin, acos, atan
 import bond
 from myformat import printrow
-from mytests import testequal
+from mytests import testequal, testeql
 
 # add common strings to include in autocomplete
 
