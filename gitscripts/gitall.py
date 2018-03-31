9fbe649368f8223315b0942443f8bbe01aad65f6
Heitor Chang <heitorchang@gmail.com>
2018-03-31 20:05:51 -0300

    save git pullall

---
+++ gitscripts/gitall.py
@@ -0,0 +1,11 @@
+GIT_REPOS = [
+    'reading-list',
+    'code-practice',
+    # 'sistema',
+    # 'pangloss',
+    # 'certitudecents',
+    # 'sistema',
+    # 'zeno',
+]
+
+HOME_DIR = '/home/heitor/'
