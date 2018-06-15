9204a29dd945a5bc67750e446f862ec11583f2c7
Heitor Chang <heitorchang@gmail.com>
2018-06-15 13:17:47 -0300

    format output

--- python/my-modules/myunittest.py
+++ python/my-modules/myunittest.py
@@ -18,8 +18,10 @@ test(
 """
 
 def test(*pair_args):
+    """https://stackoverflow.com/questions/18084476/is-there-a-way-to-use-python-unit-test-assertions-outside-of-a-testcase"""
+    
     trial = pair_args[::2]
     expected = pair_args[1::2]
     tc = unittest.TestCase('__init__')
     tc.assertEqual(trial, expected)
-    print("* All tests passed (^o^) *")
+    print("\n\n* All tests passed \\(^o^)/ \n")
