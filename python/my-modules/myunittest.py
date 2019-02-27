aa9baf3f08a7f6a6493bb90a2acc5820121e74b6
Heitor Chang <heitorchang@gmail.com>
2019-02-26 21:40:43 -0300

    test float

--- python/my-modules/myunittest.py
+++ python/my-modules/myunittest.py
@@ -20,8 +20,14 @@ test(
 def test(*pair_args):
     """https://stackoverflow.com/questions/18084476/is-there-a-way-to-use-python-unit-test-assertions-outside-of-a-testcase"""
     
+    tc = unittest.TestCase('__init__')
+
     trial = pair_args[::2]
     expected = pair_args[1::2]
-    tc = unittest.TestCase('__init__')
-    tc.assertEqual(trial, expected)
+    for t, e in zip(trial, expected):
+        if isinstance(t, float):
+            tc.assertAlmostEqual(t, e)
+        else:
+            tc.assertEqual(t, e)
+            
     print("\n\n* All tests passed \\(^o^)/ \n")
